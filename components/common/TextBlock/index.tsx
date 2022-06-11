694a4bca31ca712c2a3b3d6ee1e4e2a86cb4f802
devDohyun <dohyun10101@gmail.com>
2022-06-11 14:01:48 +0900

    feat/ added prop preset on TextBlock component

--- components/common/TextBlock/index.tsx
+++ components/common/TextBlock/index.tsx
@@ -1,17 +1,38 @@
+import { StyledTextBlockProps } from '@/components/common/TextBlock/@types'
 import { StyledTextBlock } from '@/components/common/TextBlock/styles'
+import { useMemo } from 'react'
 
 type TextBlockProps = {
   children?: string
+  preset?: string
   color?: string
   backgroundColor?: string
 }
 
-const TextBlock = ({ children, color, backgroundColor }: TextBlockProps) => {
-  return (
-    <StyledTextBlock color={color} backgroundColor={backgroundColor}>
-      {children}
-    </StyledTextBlock>
-  )
+interface IPresetMapped {
+  [key: string]: StyledTextBlockProps
+}
+
+const presetMapped: IPresetMapped = {
+  javascript: {
+    color: '#000000',
+    backgroundColor: '#ecdb54',
+  },
+}
+
+const TextBlock = ({ children, preset, color, backgroundColor }: TextBlockProps) => {
+  const textBlockProps = useMemo(() => {
+    let result: StyledTextBlockProps = {
+      color,
+      backgroundColor,
+    }
+
+    if (preset && presetMapped[preset]) result = presetMapped[preset]
+
+    return result
+  }, [preset, color, backgroundColor])
+
+  return <StyledTextBlock {...textBlockProps}>{children}</StyledTextBlock>
 }
 
 export default TextBlock
