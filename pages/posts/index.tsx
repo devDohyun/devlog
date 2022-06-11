7815be017083a8461b0c8a81bc23b2d90cea2acc
devDohyun <dohyun10101@gmail.com>
2022-06-11 18:26:25 +0900

    modified route

---
+++ pages/posts/index.tsx
@@ -0,0 +1,36 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+
+import { NextPage } from 'next'
+
+type PagePostsProps = {
+  posts?: any[]
+}
+
+const PagePosts: NextPage = ({ posts }: PagePostsProps) => {
+  return <>{JSON.stringify(posts)}</>
+}
+
+export default PagePosts
+
+export async function getStaticProps() {
+  const files = await fs.readdirSync(path.join('posts'))
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace('.md', '')
+    const markdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8')
+    const { data: frontmatter } = matter(markdownWithMeta)
+
+    return {
+      slug,
+      frontmatter,
+    }
+  })
+
+  return {
+    props: {
+      posts,
+    },
+  }
+}
