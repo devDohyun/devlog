1f916e0b612949a4f7e95bab25269c9483e08a2b
devDohyun <dohyun10101@gmail.com>
2022-06-11 17:24:44 +0900

    modified text

--- components/Home/Content/index.tsx
+++ components/Home/Content/index.tsx
@@ -59,7 +59,7 @@ const HomeContent = () => {
       <StyledSection>
         <StyledSectionTitle>코드 한 땀 한 땀 정성들여 작성하는, 24세 FrontEnd 개발자.</StyledSectionTitle>
         <StyledSectionBody>
-          특성화 고등학교 재학 중 취업 후, 현재까지 개발자로서의 커리어를 이어오고 있습니다.
+          특성화 고등학교 재학 중 취업하여, 현재까지 개발자로서의 커리어를 이어오고 있습니다.
           <br />
           Best practices와 건설적인 팀 문화를 추구하는데 있어, 그 누구보다 진심임을 자부합니다.
         </StyledSectionBody>
