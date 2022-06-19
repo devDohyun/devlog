e093e395fb2ce8aac750673fa0310582d7354ec5
devDohyun <dohyun10101@gmail.com>
2022-06-19 18:14:56 +0900

    set type for page

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,6 +1,7 @@
 import MyProfile from '@/components/MyProfile'
+import { NextPage } from 'next'
 
-const PageProfile = () => {
+const PageProfile: NextPage = () => {
   return (
     <>
       <MyProfile />
