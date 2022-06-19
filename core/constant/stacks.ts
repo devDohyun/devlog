36fc59060af5ee62d1f9e40433827b6aa342c6ef
devDohyun <dohyun10101@gmail.com>
2022-06-19 13:32:06 +0900

    added constant

---
+++ core/constant/stacks.ts
@@ -0,0 +1,11 @@
+export const myStacks = [
+  { id: 'javascript', text: 'Javascript' },
+  { id: 'next', text: 'Next.js' },
+  { id: 'nuxt', text: 'Nuxt' },
+  { id: 'react', text: 'React' },
+  { id: 'vue', text: 'Vue.js' },
+  { id: 'git', text: 'git' },
+  { id: 'scss', text: 'SCSS' },
+  { id: 'node', text: 'Node.js' },
+  { id: 'typescript', text: 'Typescript' },
+]
